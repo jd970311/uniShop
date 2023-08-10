d0602079dd3dc2e8ea3bc1eb7f4c4cacc1c03e5e
JD <jd970311@163.com>
2023-08-10 16:03:52 +0800

    组件下载以及配置

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+import uView from 'uview-ui'
+Vue.use(uView)
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
-const app = new Vue({
-  ...App
-})
+const app = new Vue({ ...App })
 app.$mount()
 // #endif
 
@@ -15,8 +15,6 @@ app.$mount()
 import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
-  return {
-    app
-  }
+  return { app }
 }
 // #endif
