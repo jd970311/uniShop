38873677188d5b7a41c23afef90eceea22f1c5fb
JD <jd970311@163.com>
2023-08-11 17:10:29 +0800

    基于uni.request封装请求

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,43 +1,13 @@
 <template>
   <view>
-    <!--  -->
-    <my-search @input="InputEvent" @focus="FocusEvent"></my-search>
-    <!-- 轮播图 -->
-    <swiper indicator-dots autoplay :interval="3000" :duration="1000">
-      <swiper-item>
-        <image src="../../static/tabbar/cart.png" mode=""></image>
-      </swiper-item>
-    </swiper>
+    <my-search></my-search>
   </view>
 </template>
 
 <script>
-  import { getData } from '@/api/home.js'
   export default {
     data() {
-      return { timer: null }
-    },
-    methods: {
-      //输入框搜索加防抖(只执行最后一次)
-      InputEvent(e) {
-        clearTimeout(this.timer)
-        this.timer = setTimeout(() => {
-          console.log(e)
-        }, 500)
-      },
-      // 聚焦事件e
-      FocusEvent() {
-        console.log(11111)
-      },
-      // 初始化数据
-      async getHomeData() {
-        const res = await getData({ pageId: 0 })
-        console.log(res, 'res')
-      },
-    },
-    mounted() {
-      // 
-      this.getHomeData()
+      return {}
     }
   }
 </script>
